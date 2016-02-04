88c51d4c96fa36613657827a2bf50ee84a57998a
MartinPaulLippert <martin@enlighten-media.net>
2016-02-04 10:36:52 +0400

    Added missing file

---
+++ TeachMeNow.DeveloperTest.BackEnd/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using TeachMeNow.DeveloperTest.BackEnd.Models;
+
+namespace TeachMeNow.DeveloperTest.BackEnd.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="System.Web.Http.ApiController" />
+    public class UsersController : ApiController
+    {
+        BackEndDB _database = null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UsersController"/> class.
+        /// </summary>
+        /// <param name="database">The database.</param>
+        public UsersController(BackEndDB database)
+        {
+            _database = database;
+        }
+
+        /// <summary>
+        /// Gets a list of Users.
+        /// </summary>
+        /// <returns>IEnumerable&lt;User&gt;</returns>
+        public IEnumerable<User> Get()
+        {
+            return _database.Users.AsEnumerable();
+        }
+
+        /// <summary>
+        /// Gets the specified user.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        public User Get(int id)
+        {
+            return _database.Users.FirstOrDefault(x => x.Id == id);
+        }
+    }
+
+}
